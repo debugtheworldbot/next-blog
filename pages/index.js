b2a7c496d11509f88889ca3154dec9bfcb4617a3
tian <istiancz@gmail.com>
2021-01-17 19:51:30 +0800

    link navigate

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,7 +12,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Fuck to <a href="https://nextjs.org">Next.js!</a>
+          Fuck to
+          <Link href="/posts/first-post">
+            <a >Next.js!</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
