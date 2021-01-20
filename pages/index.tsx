717da6651a09c19fc30411b913176af777341ee7
tian <istiancz@gmail.com>
2021-01-20 22:06:18 +0800

    create api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,29 @@
 import React from "react"
 import styles from '../styles/Home.module.css'
-import Link from "next/link";
+import Link from "next/link"
 import test from "../assets/imgs/test.png"
 
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <h1>main page</h1>
-            <img src={test} alt=""/>
+  // useEffect(()=>{
+  //   const init = async ()=>{
+  //     const res = await handler(21)
+  //   }
+  // },[])
+  return (
+    <div className={styles.container}>
+      <h1>main page</h1>
+      <img src={test} alt=""/>
 
-            <Link href={'/posts/first-post'}>
-                <a>first</a>
-            </Link>
-            <style jsx>{`
+      <Link href={'/posts/first-post'}>
+        <a>first</a>
+      </Link>
+      <style jsx>{`
               h1{
                 color: red;
               }
             `}</style>
 
-        </div>
-    )
+    </div>
+  )
 }
