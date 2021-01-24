cf3a7fc18d3d802195899f22a4e9fbc4d516b9bd
tian <istiancz@gmail.com>
2021-01-24 15:59:37 +0800

    post api

--- entity/posts.tsx
+++ entity/posts.tsx
@@ -1,14 +1,16 @@
 import path from "path"
-import fs ,{promises as fsPromise} from "fs"
+import fs, {promises as fsPromise} from "fs"
 import matter from 'gray-matter'
 
-export const getPosts = async ()=>{
-  const mdDir = path.join(process.cwd(),'markdown')
+export const getPosts = async () => {
+  const mdDir = path.join(process.cwd(), 'markdown')
   const articleList = await fsPromise.readdir(mdDir)
-  articleList.map(articleName=>{
-    const fullDir = path.join(mdDir,articleName)
-    const content = fs.readFileSync(fullDir,'utf-8')
-    console.log(matter(content))
+  return articleList.map(articleName => {
+    const fullDir = path.join(mdDir, articleName)
+    const fileName = articleName.replace(/\.md$/g, '')
+    const fulltext = fs.readFileSync(fullDir, 'utf-8')
+    const text = matter(fulltext)
+    const {data: {title, date}} = text
+    return {fileName, title, date}
   })
-  return articleList
 }
--- pages/api/v1/posts.tsx
+++ pages/api/v1/posts.tsx
@@ -4,11 +4,10 @@ import {getPosts} from '../../../entity/posts'
 
 
 const handler = async (req:NextApiRequest, res:NextApiResponse)=> {
-  const list = await getPosts()
-  console.log(list)
+  const posts = await getPosts()
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ name: 'John Doe' }))
+  res.end(JSON.stringify(posts))
 }
 
 export default handler
