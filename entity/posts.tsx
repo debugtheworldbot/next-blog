a1057a93226a6768b17bd0ad1c249fe3230ea7d6
tian <istiancz@gmail.com>
2021-01-24 15:50:18 +0800

    add gray-matter to get title

--- entity/posts.tsx
+++ entity/posts.tsx
@@ -1,5 +1,6 @@
 import path from "path"
 import fs ,{promises as fsPromise} from "fs"
+import matter from 'gray-matter'
 
 export const getPosts = async ()=>{
   const mdDir = path.join(process.cwd(),'markdown')
@@ -7,6 +8,7 @@ export const getPosts = async ()=>{
   articleList.map(articleName=>{
     const fullDir = path.join(mdDir,articleName)
     const content = fs.readFileSync(fullDir,'utf-8')
+    console.log(matter(content))
   })
   return articleList
 }
--- markdown/first-post.md
+++ markdown/first-post.md
@@ -1,5 +1,6 @@
 ---
-M1 MacBook 外接双 4k 现在用什么 Dock/软件解决方案是比较合适的？
+title: M1 MacBook 外接双 4k 现在用什么 Dock/软件解决方案是比较合适的？
+date: 2021-1-1
 ---
 
 现在是用公司的一个 ThinkPad 的 TypeC Dock，最新版本的 DisplayLink 方案，合盖状态接两个 4k60Hz 没有问题，但是系统动画有点卡顿，也有一些连接上的小 Bug 。
